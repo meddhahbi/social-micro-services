b906759a57d39d30cc437f79b76b88d38cb7cc24
Wassim-Aloui <61364693+Wassim-Aloui@users.noreply.github.com>
2023-10-18 15:37:42 +0100

    add blog's crud

---
+++ Blog/src/main/java/pione/com/Blog/blogController.java
@@ -0,0 +1,59 @@
+package pione.com.Blog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/blog")
+public class blogController {
+
+    @Autowired
+    private blogService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> save(@RequestBody blog blog){
+        service.saveBlog(blog);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Blog saved successfully!");
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String>deleteBlog(@PathVariable Integer id){
+        service.deleteBlog(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Blog deleted successfully!");
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<blog>> findAllBlogs(){
+        return ResponseEntity.ok(service.findAllBlog());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<blog> findBlog(@PathVariable Integer id) {
+        return  ResponseEntity.ok(service.findBlogById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<blog> updateBlog(@PathVariable Integer id, @RequestBody blog updatedBlog) {
+        blog existingBlog = service.findBlogById(id);
+        if (existingBlog != null) {
+
+            existingBlog.setTitre(updatedBlog.getTitre());
+            existingBlog.setDescription(updatedBlog.getAuteur());
+            existingBlog.setAuteur(updatedBlog.getAuteur());
+
+            blog updated = service.updateBlog(existingBlog);
+
+            return ResponseEntity.ok(updated);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ Blog/src/main/java/pione/com/Blog/blogService.java
@@ -0,0 +1,33 @@
+package pione.com.Blog;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class blogService {
+    @Autowired
+    private blogRepository repository;
+
+
+    public void saveBlog(blog blog){
+        repository.save(blog);
+    }
+
+    public List<blog> findAllBlog(){
+        return repository.findAll();
+    }
+
+    public void deleteBlog(Integer id) {
+        repository.deleteById(id);
+    }
+
+    public blog findBlogById(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+    public blog updateBlog(blog blog) {
+        return repository.save(blog);
+    }
+}
